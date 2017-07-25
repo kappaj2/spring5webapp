94fc35f613ab86fb935fafb33c93b9af1ce062c3
Andre Kapp <andrekapp@estalea.com>
2017-07-25 16:34:29 +0200

    Fixed bug for entity relationship

--- src/main/java/za/co/ajk/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/za/co/ajk/spring5webapp/bootstrap/DevBootstrap.java
@@ -50,13 +50,19 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
         
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        
         eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+        
         authorRepository.save(eric);
         bookRepository.save(ddd);
         
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+        
         rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+        
         authorRepository.save(rod);
         bookRepository.save(noEJB);
     }
